49689444f993e9349e570f6b2d4455b52ccef5f5
Daniel Duque <duquedk1@gmail.com>
2021-11-17 18:45:46 -0500

    refactor: App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import IcecreamList from "./components/IcecreamList";
-import { getIcecreamAxios, getIcecreamFetch } from "./api/icecream";
+import { getIcecreamFetch } from "./api/icecream";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   const [icecreams, setIcecreams] = useState([]);
 
-  
-  useEffect( async () => {
-    
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getIcecreamFetch();
+      console.log("DATA ", data);
+      setIcecreams(data);
+    }
     //const data = await getIcecreamAxios();
-    const data = await getIcecreamFetch()
-    console.log("DATA ", data)
-    setIcecreams(data);
+    fetchData();
   }, []);
 
   return (
